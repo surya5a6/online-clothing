51a7f495d515955bd533b8c6c3938770f2c42e43
tejas1 <suryateja.malisetti@pega.com>
2020-04-21 07:03:16 +0530

    Added Collection and Shop page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import React from "react";
 import "./App.css";
-import HomePage from "./homepage";
+import { Route, Switch } from "react-router-dom";
+import HomePage from "./pages/homepage/homepage";
+import ShopPage from "./pages/shoppage/shoppage";
 
 function App() {
   return (
     <div>
-      <HomePage />
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+        <Route exact path="/shop" component={ShopPage} />
+      </Switch>
     </div>
   );
 }
---
+++ src/pages/homepage/homepage.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import Directory from "../../components/directory/directory";
+import "./homepage.styles.scss";
+
+const HomePage = () => (
+  <div className="homePage">
+    <Directory />
+  </div>
+);
+
+export default HomePage;
