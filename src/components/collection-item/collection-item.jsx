feb3648eb648304360c7f8a10fbfa38779178071
tejas1 <suryateja.malisetti@pega.com>
2020-04-28 14:26:04 +0530

    Added addItem reducer

--- src/components/collection-item/collection-item.jsx
+++ src/components/collection-item/collection-item.jsx
@@ -1,14 +1,29 @@
 import React from "react";
+import { connect } from "react-redux";
+
+import { addItem } from "../../redux/cart/cart.actions";
+
+import CustomButton from "../custom-button/custom-button";
 import "./collection-item.styles.scss";
 
-const CollectionItem = ({ id, name, imageUrl, price }) => (
-  <div className="collection-item">
-    <div className="image" style={{ backgroundImage: `url(${imageUrl})` }} />
-    <div className="collection-footer">
-      <span className="name">{name}</span>
-      <span className="price">{price}</span>
+const CollectionItem = ({ item, addItem }) => {
+  const { name, imageUrl, price } = item;
+  return (
+    <div className="collection-item">
+      <div className="image" style={{ backgroundImage: `url(${imageUrl})` }} />
+      <div className="collection-footer">
+        <span className="name">{name}</span>
+        <span className="price">{price}</span>
+      </div>
+      <CustomButton onClick={() => addItem(item)} inverted>
+        Add To Cart
+      </CustomButton>
     </div>
-  </div>
-);
+  );
+};
 
-export default CollectionItem;
+const mapDispatchToProps = (dispatch) => ({
+  addItem: (item) => dispatch(addItem(item)),
+});
+
+export default connect(null, mapDispatchToProps)(CollectionItem);
--- src/redux/cart/cart.actions.js
+++ src/redux/cart/cart.actions.js
@@ -3,3 +3,8 @@ import { cartActions } from "./cart.types";
 export const toggleCartHidden = () => ({
   type: cartActions.TOGGLE_CART_HIDDEN,
 });
+
+export const addItem = (item) => ({
+  type: cartActions.ADD_ITEM,
+  payload: item,
+});
